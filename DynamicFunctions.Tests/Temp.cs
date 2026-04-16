61666b872943d8a1c475932c222745d75f9c3727
Nikita M. <mna.dvlp@gmail.com>
2026-04-17 00:25:16 +0300

    Debug test added

---
+++ DynamicFunctions.Tests/Temp.cs
@@ -0,0 +1,29 @@
+﻿using DynamicFunctions.LexicalAnalysis;
+using DynamicFunctions.LexicalAnalysis.Extensions;
+using DynamicFunctions.TextAnalysis;
+using DynamicFunctions.TextAnalysis.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DynamicFunctions.Tests;
+
+public class Temp
+{
+    [Fact]
+    public void Test1()
+    {
+        var functionText = "10 * (x + 8)^2";
+
+        var serviceProvider = new ServiceCollection()
+            .AddTextAnalysis()
+            .AddLexicalAnalysis()
+            .BuildServiceProvider();
+
+        var tokenizer = serviceProvider.GetRequiredService<ITokenizer>();
+        var lexicalAnalyzer = serviceProvider.GetRequiredService<ILexicalAnalyzer>();
+
+        var tokens = tokenizer.Tokenize(functionText).ToArray();
+        var lexicalTokens = lexicalAnalyzer.Analyze(tokens).ToArray();
+
+        int i = 0;
+    }
+}
